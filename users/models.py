937cb3bfaebc4c5f55fbb93177150961a473d74e
yhtiee <99039727+yhtiee@users.noreply.github.com>
2023-04-11 03:54:41 +0100

    authentication

--- users/models.py
+++ users/models.py
@@ -1,74 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
-class UserManager(BaseUserManager):
-    def _create_user(
-        self, email, password, is_staff, is_superuser, is_admin, **extra_fields
-    ):
-        if not email:
-            raise ValueError("Users must have an email address")
-        now = timezone.now()
-        email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            is_staff=is_staff,
-            is_active=True,
-            is_superuser=is_superuser,
-            is_admin=is_admin,
-            last_login=now,
-            date_joined=now,
-            **extra_fields,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password, **extra_fields):
-        return self._create_user(email, password, False, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        user = self._create_user(email, password, True, True, True, **extra_fields)
-        user.save(using=self._db)
-        return user
-
-    def create_admin(self, email, password, **extra_fields):
-        user = self._create_user(
-            email=email,
-            password=password,
-            is_staff=False,
-            is_superuser=False,
-            is_admin=True,
-            **extra_fields,
-        )
-        return user
-
-
-class User(AbstractBaseUser):
-    email = models.EmailField(max_length=300)
-    username = models.CharField(max_length=254, blank=False, unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_profile')
     description = models.CharField(max_length=255, null=False)
     billing_country = models.CharField(max_length=255, null=False)
-    date_joined = models.DateTimeField(default=timezone.now)
-    last_login = models.DateTimeField(null=True, blank=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_child_node = models.BooleanField(default=False)
 
-    USERNAME_FIELD = "username"
-    EMAIL_FIELD = "email"
-    REQUIRED_FIELDS = ["email"]
-
-    objects = UserManager()
-
-    def __str__(self):
-        return self.username
-
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}".strip()
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import *
+from rest_framework import status
+from django.contrib.auth.hashers import make_password
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["GET"])
+def getRoutes(request):
+    routes = [
+        "/api/token",
+        "/api/token/refresh"
+    ]
+    return Response(routes)
+
+
+class SignUp(APIView):
+    def post(self, request):
+        serializer = UserAuthSerializer(data=request.data)
+        if serializer.is_valid():
+            password = serializer.validated_data.get('password')
+            serializer.validated_data['password'] = make_password(password)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
